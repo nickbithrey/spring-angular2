f5c54836d8ba3fb401d5381a668d2da5e4169f84
Nicholas Bithrey <Nicholas Bithrey@WIN-1P6OKMH4CL8.Home>
2016-09-02 18:39:43 +0100

    Initial commit

---
+++ angular2/src/main/java/com/angular2/configuration/ApplicationContext.java
@@ -0,0 +1,22 @@
+package com.angular2.configuration;
+
+//@Configuration
+//@EnableWebMvc
+public class ApplicationContext {// extends WebMvcConfigurerAdapter {
+	//
+	// @Bean
+	// public ViewResolver getViewResolver() {
+	// InternalResourceViewResolver resolver = new
+	// InternalResourceViewResolver();
+	// resolver.setPrefix("/WEB-INF/");
+	// resolver.setSuffix(".html");
+	// return resolver;
+	// }
+	//
+	// @Override
+	// public void
+	// configureDefaultServletHandling(DefaultServletHandlerConfigurer
+	// configurer) {
+	// configurer.enable();
+	// }
+}
---
+++ angular2/src/main/webapp/app/main.ts
@@ -0,0 +1,8 @@
+//import {bootstrap} from 'static/node_modules/angular2/platform/browser.js';
+//import {AppComponent} from 'static/app/app.component.js';
+
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+console.log(bootstrap);
+bootstrap(AppComponent);
