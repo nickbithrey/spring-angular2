65a9801b34c46b1f655f920914089b8eb25aa3a5
Nicholas Bithrey <Nicholas Bithrey@WIN-1P6OKMH4CL8.Home>
2016-09-03 09:14:58 +0100

    Spring Boot setup. Removed unused files.

--- angular2/src/main/java/com/angular2/Application.java
+++ angular2/src/main/java/com/angular2/Application.java
@@ -2,43 +2,13 @@ package com.angular2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
-
-	private static Class<Application> applicationClass = Application.class;
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@Override
-	public void customize(ConfigurableEmbeddedServletContainer container) {
-		// container.setDocumentRoot(new File("index.html"));
-	}
-
-	// @Bean
-	// public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	// return args -> {
-	//
-	// System.out.println("Let's inspect the beans provided by Spring Boot:");
-	//
-	// String[] beanNames = ctx.getBeanDefinitionNames();
-	// Arrays.sort(beanNames);
-	// for (String beanName : beanNames) {
-	// System.out.println(beanName);
-	// }
-	//
-	// };
-	// }
-
-	// @Override
-	// protected SpringApplicationBuilder configure(SpringApplicationBuilder
-	// application) {
-	// return application.sources(applicationClass);
-	// }
 
 }
