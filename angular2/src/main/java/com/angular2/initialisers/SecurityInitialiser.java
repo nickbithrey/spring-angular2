96b8134f81fe413cb6da45e0d8e72a9112d40cdd
Nicholas Bithrey <nickbithrey@hotmail.co.uk>
2016-09-12 21:07:36 +0100

    add spring security default login page

---
+++ angular2/src/main/java/com/angular2/initialisers/SecurityInitialiser.java
@@ -0,0 +1,7 @@
+package com.angular2.initialisers;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitialiser extends AbstractSecurityWebApplicationInitializer {
+
+}
